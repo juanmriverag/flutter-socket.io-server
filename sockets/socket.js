e8de0565846089544d352775b96c515ec71b9c10
Juan Manuel Rivera G <jumriver@bancolombia.com.co>
2023-06-14 23:37:21 -0500

    ap terminada

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,10 +1,36 @@
 const {io} = require('../index');
 
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('can'));
+bands.addBand(new Band('Santa'));
+bands.addBand(new Band('Mc'));
+
 //Messages Sockets
 io.on('connection', client => {
     console.log('client connect')
+
+    client.emit('active-bands', bands.getBands());
     client.on('disconnect', () => { console.log('client disconnect')});
-    client.on('mensaje',(payload)=>{
-      console.log('Mensaje!!!',payload);
-    });
+
+
+    client.on('vote-band',(data)=>{
+      bands.voteBand(data.id);
+      io.emit('active-bands', bands.getBands());
+      });
+
+      client.on('new-band',(data)=>{
+        const newBand = new Band(data.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+        });
+
+        client.on('delete-band',(data)=>{
+         bands.deleteBand(data.id)
+          io.emit('active-bands', bands.getBands());
+          });
   });
